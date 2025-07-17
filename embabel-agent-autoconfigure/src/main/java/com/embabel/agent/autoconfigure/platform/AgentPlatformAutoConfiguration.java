66a413590d03f93c64a871af8c26953db509b100
Iván Rodríguez Murillo <wantez@gmail.com>
2025-07-18 00:10:06 +0100

    Add `RuntimeHintsConfig` and `PersonalityResourcesRuntimeHints` for GraalVM native compatibility. Not verified
    Signed-off-by: Iván Rodríguez Murillo <wantez@gmail.com>

--- embabel-agent-autoconfigure/src/main/java/com/embabel/agent/autoconfigure/platform/AgentPlatformAutoConfiguration.java
+++ embabel-agent-autoconfigure/src/main/java/com/embabel/agent/autoconfigure/platform/AgentPlatformAutoConfiguration.java
@@ -20,7 +20,6 @@ import com.embabel.agent.config.AgentPlatformConfiguration;
 import com.embabel.agent.config.RuntimeHintsConfig;
 import com.embabel.agent.config.RagServiceConfiguration;
 import com.embabel.agent.config.ToolGroupsConfiguration;
-import com.embabel.common.util.UtilRuntimeHints;
 import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -29,7 +28,6 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.ImportRuntimeHints;
 
 /**
  * Bootstraps Agent Platform Configuration, Tools Configuration, and Rag Service Configuration
@@ -46,8 +44,7 @@ import org.springframework.context.annotation.ImportRuntimeHints;
         }
 )
 @ConditionalOnClass({AgentPlatformConfiguration.class, ToolGroupsConfiguration.class, RagServiceConfiguration.class})
-@Import({AgentPlatformConfiguration.class, ToolGroupsConfiguration.class, RagServiceConfiguration.class})
-@ImportRuntimeHints(UtilRuntimeHints.class)
+@Import({AgentPlatformConfiguration.class, ToolGroupsConfiguration.class, RagServiceConfiguration.class, RuntimeHintsConfig.class})
 public class AgentPlatformAutoConfiguration {
     final private Logger logger = LoggerFactory.getLogger(AgentPlatformAutoConfiguration.class);
 
